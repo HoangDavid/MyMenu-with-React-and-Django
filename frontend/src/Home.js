d71db2b86e55e2adbbd0932f03fbf6c965860a0d
DavidLoveSpace <davidlovespace@Davids-MacBook-Air.local>
2024-01-09 18:16:42 -0500

    adding someting here

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,19 +2,30 @@ import './Home.css'
 
 function Home(){
     return (
-        <div class="container">
-            <div class="header">
-                <div class="item"><i class="logo"></i></div>
-                <div class="item">Comunity</div>
-                <div class="item">Make your own</div>
-                <div class="item">FAQ</div>
-                <div class="item">
-                    <i class="search-icon"></i>
-                    <input id="search" type="text" placeholder="search for templates"></input>
+        <span>
+            <div class="container">
+                <div class="header">
+                    <div class="item"><i class="logo"></i></div>
+                    <div class="item">Community</div>
+                    <div class="item">Make your own</div>
+                    <div class="item">FAQ</div>
+                    <div class="item">
+                        <i class="search-icon"></i>
+                        <input id="search" type="text" placeholder="search for templates"></input>
+                    </div>
+                    <div class="item"><i class="profile"></i></div>
                 </div>
-                <div class="item"><i class="profile"></i></div>
             </div>
-        </div>
+
+            
+            <div class="container">
+                <div class="slider">
+                    <div class="slide-item">
+                        <img id="card1"/>
+                    </div>
+                </div>
+            </div>
+        </span>
     )
 }
 
