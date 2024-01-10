9f538f7cb3d252f617fd7182000621b30229ec8b
DavidLoveSpace <davidlovespace@Davids-MacBook-Air.local>
2024-01-10 11:19:11 -0500

    changing some stuffs

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,29 +1,29 @@
 import './Home.css'
+import React from 'react'
 
 function Home(){
+
+
+
     return (
         <span>
-            <div class="container">
-                <div class="header">
-                    <div class="item"><i class="logo"></i></div>
-                    <div class="item">Community</div>
-                    <div class="item">Make your own</div>
-                    <div class="item">FAQ</div>
-                    <div class="item">
-                        <i class="search-icon"></i>
+            <div className="container">
+                <div className="header">
+                    <div className="item"><i className="logo"></i></div>
+                    <div className="item">Community</div>
+                    <div className="item">Make your own</div>
+                    <div className="item">FAQ</div>
+                    <div className="item">
+                        <i className="search-icon"></i>
                         <input id="search" type="text" placeholder="search for templates"></input>
                     </div>
-                    <div class="item"><i class="profile"></i></div>
+                    <div className="item"><i className="profile"></i></div>
                 </div>
             </div>
 
             
-            <div class="container">
-                <div class="slider">
-                    <div class="slide-item">
-                        <img id="card1"/>
-                    </div>
-                </div>
+            <div className="container">
+                <img src='images/slides/1.jpg' alt='1'/>
             </div>
         </span>
     )
